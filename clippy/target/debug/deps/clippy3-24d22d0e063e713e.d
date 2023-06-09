09d6e26d90fcc3b5ca3379ecffdd589e540ef4eb
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2023-06-08 21:18:27 -0400

    initial commit

---
+++ clippy/target/debug/deps/clippy3-24d22d0e063e713e.d
@@ -0,0 +1,8 @@
+/home/xanderjakeq/dev/rustlings/exercises/clippy/target/debug/deps/clippy3-24d22d0e063e713e.rmeta: clippy3.rs Cargo.toml
+
+/home/xanderjakeq/dev/rustlings/exercises/clippy/target/debug/deps/clippy3-24d22d0e063e713e.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
